9e68fce39b02821f8e7736c362d696c66a788c9f
surshi <ushithila@gmail.com>
2026-07-16 12:52:09 -0400

    variable change + svg

--- scripts/script.js
+++ scripts/script.js
@@ -2,4 +2,14 @@
 // function  for date 
 // function for html 
 
-import { getDirectory } from "./service.storage.js"
+import entries from './directory-data.js'
+
+
+const table_entries = document.getElementById('table-body')
+
+entries.forEach(element => {
+    const folderName = document.createElement();
+    const uploadDate = document.createElement();
+    const folderSize = document.createElement();
+    const 
+});
