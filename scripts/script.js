50639f46ead700eeeab954843898c5e4b4d554bd
surshi <ushithila@gmail.com>
2026-07-16 16:22:39 -0400

    start on js

--- scripts/script.js
+++ scripts/script.js
@@ -2,14 +2,27 @@
 // function  for date 
 // function for html 
 
-import entries from './directory-data.js'
+import entries from './directory-data.js';
 
 
-const table_entries = document.getElementById('table-body')
+const table_entries = document.getElementById('table-body');
 
 entries.forEach(element => {
-    const folderName = document.createElement();
-    const uploadDate = document.createElement();
-    const folderSize = document.createElement();
-    const 
+    const newRow = document.createElement('tr');
+    newRow.setAttribute();
+
+    const checkBox = document.createElement('td');
+    checkBox.setAttribute('class', 'checkbox');
+
+    const folderName = document.createElement('td');
+    folderName.setAttribute('class', 'td');
+
+    const uploadDate = document.createElement('td');
+    uploadDate.setAttribute('class', 'td');
+
+    const folderSize = document.createElement('td');
+    folderSize.setAttribute('class', 'td');
+
+    const actionDropdown = document.createElement('td');
+    actionDropdown.setAttribute('class', 'action-td');
 });
