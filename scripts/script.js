6fb43a21acf48fa0f4481a877c5598216c79e67f
surshi <ushithila@gmail.com>
2026-07-17 12:25:26 -0400

    testing object to array conversion

--- scripts/script.js
+++ scripts/script.js
@@ -1,44 +1,46 @@
-// import { getDirectory  } from './service.storage.js';
+import { getDirectory  } from './service.storage.js';
 
-// const data = await getDirectory('/');
-// console.log(data);
+const data = await getDirectory('/');
 
+Array.from(data).forEach(element => {
+    console.log(element);
+});
 
-function loadData(data){
-    const table_entries = document.getElementById('table-body');
-    const newRow = document.createElement('tr');
-    const checkboxCol = document.createElement('td');
-    const checkbox = document.createElement('input');
-    const folderName = document.createElement('td');
-    const uploadDate = document.createElement('td');
-    const folderSize = document.createElement('td');
-    const actionColumn = document.createElement('td');
-    const actionButton = document.createElement('button');
-    const actionIcon = document.createElement('i');
+// function loadData(data){
+//     const table_entries = document.getElementById('table-body');
+//     const newRow = document.createElement('tr');
+//     const checkboxCol = document.createElement('td');
+//     const checkbox = document.createElement('input');
+//     const folderName = document.createElement('td');
+//     const uploadDate = document.createElement('td');
+//     const folderSize = document.createElement('td');
+//     const actionColumn = document.createElement('td');
+//     const actionButton = document.createElement('button');
+//     const actionIcon = document.createElement('i');
 
-    checkbox.type = 'checkbox';
-    checkbox.className = 'checkbox';
-    actionButton.type = 'button';
-    actionButton.className = 'action-button';
-    actionIcon.className = "fa-solid fa-ellipsis-vertical";
+//     checkbox.type = 'checkbox';
+//     checkbox.className = 'checkbox';
+//     actionButton.type = 'button';
+//     actionButton.className = 'action-button';
+//     actionIcon.className = "fa-solid fa-ellipsis-vertical";
     
-    newRow.setAttribute('class', 'table-row');
-    checkboxCol.setAttribute( 'class', 'checkbox-tr');
-    actionColumn.setAttribute( 'class', 'action-td');
+//     newRow.setAttribute('class', 'table-row');
+//     checkboxCol.setAttribute( 'class', 'checkbox-tr');
+//     actionColumn.setAttribute( 'class', 'action-td');
     
-    checkboxCol.append(checkbox);
-    folderName.append(element.name);
-    uploadDate.append(element.createdAt);
-    folderSize.append(element.size);
-    actionButton.append(actionIcon);
-    actionColumn.append(actionButton);
-    newRow.append(checkboxCol);
-    newRow.append(folderName);
-    newRow.append(uploadDate);
-    newRow.append(folderSize);
-    newRow.append(actionColumn);
-    table_entries.append(newRow);
-}
+//     checkboxCol.append(checkbox);
+//     folderName.append(data.name);
+//     uploadDate.append(data.createdAt);
+//     folderSize.append(data.size);
+//     actionButton.append(actionIcon);
+//     actionColumn.append(actionButton);
+//     newRow.append(checkboxCol);
+//     newRow.append(folderName);
+//     newRow.append(uploadDate);
+//     newRow.append(folderSize);
+//     newRow.append(actionColumn);
+//     table_entries.append(newRow);
+// }
 
 
 
